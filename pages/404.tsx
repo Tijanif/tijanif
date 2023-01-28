fee87b48645d14483709390ca708ef80b34eab9f
Tijani Fulani <Fulani07@gmail.com>
2023-01-28 18:32:05 +0100

    added more style on the 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,15 +2,17 @@ import Link from "next/link";
 
 const PageNotFound = () => {
   return (
-      <div>
-          <h1>Ooooops....</h1>
-          <h2>That page can not be found</h2>
-          <p>Go back to the   <Link href="/">
-              <a>Home Page</a>
-          </Link>  </p>
-      </div>
-  )
-}
-
+    <div>
+      <h1>Ooooops....</h1>
+      <p>That page can not be found🛑</p>
+      <p>
+        Go back to the 🏡{" "}
+        <Link href='/'>
+          <a>Home Page</a>
+        </Link>{" "}
+      </p>
+    </div>
+  );
+};
 
 export default PageNotFound;
